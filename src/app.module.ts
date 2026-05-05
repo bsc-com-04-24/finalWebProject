73effb6a560bd96273c2f86b4ef7bb8462534968
bsc-com-04-24 <bsc-com-04-24@unima.ac.mw>
2026-05-05 05:02:08 -0700

    created resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { CartModule } from './cart/cart.module';
+import { ServiceModule } from './service/service.module';
+import { OrderModule } from './order/order.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [ProductModule, CartModule, ServiceModule, OrderModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
