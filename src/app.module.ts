8e9afb6791a10242bd980f447980423ac7bbd978
bsc-inf-24-24 <bsc-inf-24-24@unima.ac.mw>
2026-05-06 14:40:23 -0700

    Updated the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,3 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CartsModule } from './cart/cart.module';
 
-@Module({
-  imports: [CartsModule],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+
+
