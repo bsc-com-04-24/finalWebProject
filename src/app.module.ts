e91fcd35df34d8f258c0a20bc344e7c71abb54a6
bsc-inf-24-24 <bsc-inf-24-24@unima.ac.mw>
2026-05-06 15:37:38 -0700

    Updated app modele

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CartsModule } from './cart/cart.module';
+import { Cart } from './cart/entities/cart.entity';
 
 
+@Module({
+imports: [
+ConfigModule.forRoot({ isGlobal: true }),
+TypeOrmModule.forRootAsync({
+imports: [ConfigModule],
+inject: [ConfigService],
+useFactory: (config: ConfigService) => ({
+type: 'oracle',
+host: config.get('DB_HOST'),
+port: parseInt(config.get('DB_PORT') ?? '1521'),
+username: config.get('DB_USERNAME'),
+password: config.get('DB_PASSWORD'),
+serviceName: config.get('DB_SERVICE_NAME'),
+synchronize: config.get('DB_SYNCHRONIZE') === 'true',
+entities: [Cart],
+logging: true,
+}),
+}),
 
+CartsModule,
+],
+})
+export class AppModule {}
